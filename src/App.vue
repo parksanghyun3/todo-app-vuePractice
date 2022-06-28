e9bfcf277f1f2387a1b4f99768498db63ac6a049
PARKSANGHYUN3 <basira@easymedia.net>
2022-06-28 09:53:10 +0900

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <TodoHeader />
+    <TodoInput />
+    <TodoList />
+    <TodoFooter />
+  </div>
+</template>
+
+<script>
+import TodoHeader from "./components/TodoHeader.vue"
+import TodoInput from "./components/TodoInput.vue"
+import TodoList from "./components/TodoList.vue"
+import TodoFooter from "./components/TodoFooter.vue"
+
+export default {
+  name: 'App',
+  components: {
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
+  }
+}
+</script>
+
+<style>
+  body {width: 300px; margin: 0 auto; text-align: center; background: linear-gradient(to right, #6478FB, #8763FB)}
+  input {border-style : groove; width: 200px; border: 0;}
+  button {border-style : groove;}
+  .shadow {box-shadow: 5px 10px 10px rgba(0,0,0,0.03);}
+</style>
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Todo It!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  h1 {color : #fff; font-weight : 900; margin : 2.5rem 0 1.5rem;}
+</style>
